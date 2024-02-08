4e4a0b91acf701a32718bba471c24376100ad38e
Aaron Echler <acechler@gmail.com>
2024-02-08 17:55:12 -0500

    Create sinewave example

--- p5-core/tutorial/sketch.js
+++ p5-core/tutorial/sketch.js
@@ -6,6 +6,11 @@ function setup() {
 
 function draw() {
   let mySineWave = new SineWave(50, 1, 0); // Example: amplitude 50, frequency 1, phase 0
+  let mySineWave2 = new SineWave(50, 1, 3); // Example: amplitude 50, frequency 1, phase 0
+  let mySineWave3 = new SineWave(-50, 3, 0); // Example: amplitude 50, frequency 1, phase 0
+
   mySineWave.display();
+  mySineWave2.display();
+  mySineWave3.display();
 }
 
