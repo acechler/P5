ccb7cee7ca3af6b4ca21f7bd71523e2bde4c08da
Aaron Echler <acechler@gmail.com>
2024-02-11 16:48:45 -0500

    Create new documentation directory and add custom shape markdown file

--- p5-core/tutorial/sketch.js
+++ p5-core/tutorial/sketch.js
@@ -9,9 +9,9 @@ function draw() {
     background(0);
 
     for(let x = 0; x < 25; x++){
-
-        rect(30+x, 20+x*2, 60, 60); // x y w h
+        rect(30+x, 30+x, 60+x, 60+x); // x y w h
     }
+
 }
   
   
