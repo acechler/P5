bec73542baab059d6c0e444f11a2bf1eaec6c867
Aaron Echler <acechler@gmail.com>
2024-02-11 13:41:04 -0500

    Move circle example into its own directory, create a new base template.

--- p5-core/tutorial/sketch.js
+++ p5-core/tutorial/sketch.js
@@ -1,36 +1,11 @@
 
-class Circle{
-    constructor(x,y, size){
-        this.x = x;
-        this.y = y;
-        this.size = size;
-    }
 
-    resetPosition(){
-        this.x = 0;
-        this.y = 0;
-    }
-
-    draw(){
-        circle(this.x,this.y,this.size);
-        this.x = this.x + 5;
-        this.y = this.y + 5;        
-    }
-}
-
-const mycircle = new Circle(0,0,64);
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(400, 200);
+}
+  
+function draw() {
     background(0);
 }
-
-function mousePressed(){
-    mycircle.resetPosition();
-}
-
-function draw(){
-    background(0);
-    noStroke();
-    fill(255);
-    mycircle.draw();
-}
+  
+  
