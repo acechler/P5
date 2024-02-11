75790522e27a122d7158d021b3c4e97295aa8664
Aaron Echler <acechler@gmail.com>
2024-02-11 16:39:44 -0500

    Create a cube

--- p5-core/tutorial/sketch.js
+++ p5-core/tutorial/sketch.js
@@ -1,11 +1,17 @@
 
 
+
 function setup() {
     createCanvas(400, 200);
 }
   
 function draw() {
     background(0);
+
+    for(let x = 0; x < 25; x++){
+
+        rect(30+x, 20+x*2, 60, 60); // x y w h
+    }
 }
   
   
