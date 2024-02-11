040dd549bc6eb5a234e188978683b6b559c073d3
Aaron Echler <acechler@gmail.com>
2024-02-11 13:08:40 -0500

    Work on variable declarations

--- p5-core/tutorial/sketch.js
+++ p5-core/tutorial/sketch.js
@@ -1,19 +1,25 @@
 
 
+let circleX = 100;
+let circleY = 100;
+
+
 function setup() {
     createCanvas(400, 400);
     background(0);
 }
 
+function mousePressed(){
+    circleX = 0;
+    circleY = 0;
+}
 
 function draw(){
-
+    background(0);
     noStroke();
     fill(255);
-    circle(mouseX, mouseY, 24);
-   
-}
-// Clear the canvas
-function mousePressed(){
-    background(0);
+    circle(circleX, circleY, 64);
+
+    circleX = circleX + 5;
+    circleY = circleY + 5;
 }
