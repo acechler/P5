2897028f002d54d2bdb1b7de1db69a654c2651da
Aaron Echler <acechler@gmail.com>
2024-02-16 19:04:41 -0500

    Mouse click interaction.

--- p5-core/sandbox/sketch.js
+++ p5-core/sandbox/sketch.js
@@ -1,4 +1,5 @@
-function getCoords(x, y) {
+
+
   // Function to calculate distance from another point
   function distanceFrom(xOther, yOther) {
     return Math.sqrt((xOther - x) ** 2 + (yOther - y) ** 2);
@@ -10,38 +11,45 @@ function getCoords(x, y) {
     y = newY;
   }
 
-  return {
-    get x() {
-      return x;
-    },
-    get y() {
-      return y;
-    },
-    distanceFrom,
-    updateCoords
-  };
-}
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
 
+// Example 1-2: Bouncing Ball, with p5.Vector!
+let position;
+let velocity;
+let target;
 function setup() {
-  createCanvas(800, 800);
-  //noLoop();
+  createCanvas(640, 360);
+  background(255);
+  position = createVector(100, 100);
+  velocity = createVector(2.5, 5);
+  target = createVector(0,0);
 }
-
-let coords1 = getCoords(20, 20);
-let coords2 = getCoords(50, 5);
 
 function draw() {
-  background(220);
-    stroke(0); // This is more efficient outside the loops if it doesn't change
-    // Custom Shape
-    
-    
-    for(let i = 0; i < 2; i++){
-      beginShape();
-      vertex(coords1.x - i, coords1.y - i);
-      vertex(coords2.x - i, coords2.y - i);
-      endShape(CLOSE);
-      coords1.updateCoords(Math.sin(coords1.x+i),Math.sin(coords1.y+i))
-      coords2.updateCoords(coords2.x+i,coords2.y+i)
-    }
+  background(255);
+
+  // Add the current speed to the position.
+  position.add(velocity);
+
+  if ((position.x > width) || (position.x < 0)) {
+    velocity.x = velocity.x * -1;
+  }
+  if ((position.y > height) || (position.y < 0)) {
+    velocity.y = velocity.y * -1;
+  }
+
+  line(position.x, position.y, target.x, target.y);
+
+  // Display circle at x position
+  stroke(0);
+  strokeWeight(2);
+  fill(127);
+  ellipse(position.x, position.y, 48, 48);
+}
+
+function mousePressed(){
+  target.x = mouseX;
+  target.y = mouseY;
 }
