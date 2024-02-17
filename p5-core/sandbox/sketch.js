93703b08975bb4f3c8d571d7cac362bd686d4b81
Aaron Echler <acechler@gmail.com>
2024-02-17 16:24:58 -0500

    Created a position queue utility class.

--- p5-core/sandbox/sketch.js
+++ p5-core/sandbox/sketch.js
@@ -1,54 +1,64 @@
+class PositionQueue {
+  constructor(position, velocity) {
+    this.position = position;
+    this.velocity = velocity;
+    this.targets = [];
+  }
 
-let position;
-let velocity;
-let targets = []; // Queue to store target points
+  addPosition(newPosition) {
+    this.targets.push(newPosition);
+  }
+
+  draw() {
+    if (this.targets.length > 0) {
+      let lastElement = this.targets[this.targets.length - 1];
+      text(`${lastElement.x}, ${lastElement.y}`, 100, 50);
+
+      let target = createVector(this.targets[0].x, this.targets[0].y);
+      let direction = p5.Vector.sub(target, this.position);
+      
+      if (direction.mag() > 1) {
+        direction.setMag(2);
+        this.position.add(direction);
+      } else {
+        this.targets.shift();
+      }
+    }
+
+    stroke(0);
+    strokeWeight(2);
+    fill(127);
+    ellipse(this.position.x, this.position.y, 48, 48);
+
+    stroke(100, 100, 250, 100);
+    beginShape();
+    vertex(this.position.x, this.position.y);
+    for (let target of this.targets) {
+      vertex(target.x, target.y);
+    }
+    endShape();
+  }
+}
+
+let moveQueue;
 
 function setup() {
   createCanvas(640, 360);
   background(255);
-  position = createVector(100, 100); // Starting position
-  velocity = createVector(0, 0); // Initial velocity is zero
-  
+  let position = createVector(100, 100);
+  let velocity = createVector(0, 0);
+  moveQueue = new PositionQueue(position, velocity); 
   frameRate(60);
-  
+
   textSize(30);
   textAlign(CENTER, CENTER);
 }
+
 function draw() {
   background(255);
-  
-  if (targets.length > 0) {
-    let lastElement = targets[targets.length - 1];
-    // Display the last target's X coordinate only if there is at least one target
-    text(`${lastElement.x}, ${lastElement.y}`, 100, 50);
-
-    let target = createVector(targets[0].x, targets[0].y); // Current target is the first item in the queue
-    let direction = p5.Vector.sub(target, position); // Calculate direction towards the target
-    
-    if (direction.mag() > 1) {
-      direction.setMag(2); // Control the speed of movement
-      position.add(direction); // Move towards the target
-    } else {
-      targets.shift(); // Remove the reached target from the queue
-    }
-  }
-  
-  // Display the shape at the current position
-  stroke(0);
-  strokeWeight(2);
-  fill(127);
-  ellipse(position.x, position.y, 48, 48);
-  
-  // Optionally, draw lines to all targets in the queue
-  stroke(100, 100, 250, 100); // Light blue with some transparency
-  beginShape();
-  vertex(position.x, position.y);
-  for (let target of targets) {
-    vertex(target.x, target.y);
-  }
-  endShape();
+  moveQueue.draw();
 }
 
 function mousePressed() {
-  targets.push({ x: mouseX, y: mouseY }); // Add a new target point to the queue
+  moveQueue.addPosition({x: mouseX, y: mouseY});
 }
