015e9145fadf3b7f50f1152be352215633299893
Aaron Echler <acechler@gmail.com>
2024-02-17 19:50:07 -0500

    Solve bug

--- p5-core/sandbox/sketch.js
+++ p5-core/sandbox/sketch.js
@@ -5,12 +5,13 @@ class Human {
     this.velocity = createVector(velocity, velocity); // Creating vectors for position & velocity
     
     this.movementQueue = new PositionQueue(this.position, this.velocity); // Now a property of Human
-    this.movementQueue.enableShape(true);
+    this.movementQueue.enableShape(false);
     this.movementQueue.enableTrail(false);
   }
 
   draw() {
     // Corrected to use properties of the Human class
+    this.movementQueue.draw();
     rect(this.position.x, this.position.y, 50, 50); // Corrected to use Human's position
   }
 }
@@ -20,12 +21,12 @@ let testHuman;
 function setup() {
   createCanvas(400, 200);
   // Create a Human instance correctly. Assuming the parameters are intended to be initial positions.
-  testHuman = new Human(25, 25); // Corrected to match Human constructor
+  testHuman = new Human(); // Corrected to match Human constructor
 }
 
 function draw() {
   background(255);
-  testHuman.movementQueue.draw();
+  testHuman.draw();
 }
 function mousePressed() {
   testHuman.movementQueue.addPosition({x: mouseX, y: mouseY});
