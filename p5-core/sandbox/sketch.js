e0ed2070c8990369040b5e9e3b41be056efc1728
Aaron Echler <acechler@gmail.com>
2024-02-17 20:00:54 -0500

    Make adjustments.

--- p5-core/sandbox/sketch.js
+++ p5-core/sandbox/sketch.js
@@ -10,9 +10,22 @@ class Human {
     this.movementQueue.enableTrail(false);
   }
 
+
+  debug(flag){
+    if(flag === false){
+      this.movementQueue.enableTrail(false)
+      this.movementQueue.enableDrawPositionText(false);
+      return;
+    } else {
+      this.movementQueue.enableTrail(true)
+      this.movementQueue.enableDrawPositionText(true);
+    }
+  }
+
   draw() {
     // Corrected to use properties of the Human class
     this.movementQueue.draw();
+    fill(1);
     rect(this.position.x, this.position.y, 8, 8); // Corrected to use Human's position
   }
 }
@@ -20,9 +33,10 @@ class Human {
 let testHuman;
 
 function setup() {
-  createCanvas(400, 200);
+  createCanvas(800, 800);
   // Create a Human instance correctly. Assuming the parameters are intended to be initial positions.
   testHuman = new Human(); // Corrected to match Human constructor
+  testHuman.debug(true);
 }
 
 function draw() {
