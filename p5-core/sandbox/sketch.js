dc7e1bf46acc55a2435cd59e873a8007a3802865
Aaron Echler <44124054+acechler@users.noreply.github.com>
2024-03-08 16:56:47 -0500

    Setup sandbox

--- p5-core/sandbox/sketch.js
+++ p5-core/sandbox/sketch.js
@@ -1,33 +1,17 @@
-// create a bouncing ball
-let ball = {
-    x: 100,
-    y: 100,
-    diameter: 50,
-    xSpeed: 3,
-    ySpeed: 2
-};
+function getFrameCountRate(){
+    return frameCount * 0.01;
+}
 
 function setup() {
     createCanvas(400, 400);
-}
+    rectMode(CENTER);
+    angleMode(DEGREES);
 
+
+}
+  
 function draw() {
-    background(0);
+    background(220);
 
-    // Draw the ball
-    fill(255);
-    noStroke();
-    ellipse(ball.x, ball.y, ball.diameter, ball.diameter);
-
-    // Update the ball's position
-    ball.x += ball.xSpeed;
-    ball.y += ball.ySpeed;
-
-    // Check for bouncing
-    if (ball.x < ball.diameter / 2 || ball.x > width - ball.diameter / 2) {
-        ball.xSpeed *= -1;
-    }
-    if (ball.y < ball.diameter / 2 || ball.y > height - ball.diameter / 2) {
-        ball.ySpeed *= -1;
-    }
 }
+  
