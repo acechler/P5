0a2feb2d5b29845d72edc8b4857050fbf0582007
Aaron Echler <44124054+acechler@users.noreply.github.com>
2024-03-08 17:04:45 -0500

    Setup grid

--- p5-core/sandbox/sketch.js
+++ p5-core/sandbox/sketch.js
@@ -1,18 +1,36 @@
+let distMouse = 50;
+let cols; let rows;
+let blockSize = 64;
+let blockOffset = 4;
+let blocks = [];
+
 function getFrameCountRate(){
     return frameCount * 0.01;
 }
 
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(800, 800);
     rectMode(CENTER);
     angleMode(DEGREES);
+    cols = width/blockSize;
+    rows = height/blockSize;
+    for(let i = 0; i < cols; i++){
+        blocks[i] = [];
+        for(let j = 0; j < rows; j++){
+            blocks[i][j] = new Block(blockSize/2 + i*blockSize, blockSize/2 + j*blockSize);
+            blocks[i][j].setColor('gray');
+        }
+    }
 
-    
 }
   
 function draw() {
-    background(220);
-
-
+    background(0);
+    for(let i = 0; i < cols; i++){
+        for(let j = 0; j < rows; j++){
+            blocks[i][j].move();
+            blocks[i][j].draw();
+        }
+    }
 }
   
